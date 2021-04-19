53ec1213e53fccda0144777f81e1df36d04238d7
Erik Fanki <erik.fanki@aventyret.com>
2021-04-19 18:31:44 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Form from './components/Form/Form';
+import Result from './components/Result/Result';
+
+import './shakes.css';
 
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [gender, setGender] = useState('');
+  const [level, setLevel] = useState(3);
+  const [mood, setMood] = useState('');
+
+  const [currentStep, setCurrentStep] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'App level' + level}>
+      <h1>Find your <span className="accent">Gen Z</span> streamer name</h1>
+      {isSubmitted ? <Result setIsSubmitted={setIsSubmitted} gender={gender} setGender={setGender} level={level} setLevel={setLevel} mood={mood} setMood={setMood} setCurrentStep={setCurrentStep}/> : <Form currentStep={currentStep} setCurrentStep={setCurrentStep} gender={gender} level={level} mood={mood} setMood={setMood} setIsSubmitted={setIsSubmitted} setGender={setGender} setLevel={setLevel} />}
     </div>
   );
 }
